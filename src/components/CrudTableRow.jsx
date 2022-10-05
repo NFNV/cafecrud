ad857b1bd29f8537b571daf833ca530853d88e43
Nicolás Valiconto <nvaliconto@outlook.com>
2022-10-05 14:53:47 -0300

    crudtablerow done

---
+++ src/components/CrudTableRow.jsx
@@ -0,0 +1,14 @@
+const CrudTableRow = () => {
+  return (
+    <tr>
+      <td>Cappuccino</td>
+      <td>Apple roll</td>
+      <td>
+        <button>Edit</button>
+        <button>Delete</button>
+      </td>
+    </tr>
+  )
+}
+
+export default CrudTableRow
