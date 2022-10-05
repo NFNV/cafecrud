e6cbe345d8532b98426a6acc85807c81ebfc1a51
Nicolás Valiconto <nvaliconto@outlook.com>
2022-10-05 14:42:30 -0300

    crudtable finished

---
+++ src/components/CrudTable.jsx
@@ -0,0 +1,28 @@
+const CrudTable = () => {
+  return (
+    <div>
+      <h3>Tabla de Datos</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Beverage</th>
+            <th>Food</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Cappuccino</td>
+            <td>Apple roll</td>
+            <td>
+              <button>Edit</button>
+              <button>Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default CrudTable
